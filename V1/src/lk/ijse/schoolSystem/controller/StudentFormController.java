d0b9e25b99a70b80d726b42e542a6d3185a35639
seekerscloudlk <seekerscloudlkinfo@gmail.com>
2020-08-09 11:09:57 +0530

    created UI and Controller

---
+++ V1/src/lk/ijse/schoolSystem/controller/StudentFormController.java
@@ -0,0 +1,27 @@
+package lk.ijse.schoolSystem.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class StudentFormController {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtAge;
+    public TableView tbl;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colAge;
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+}
